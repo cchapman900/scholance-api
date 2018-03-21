0243156adfd0f6dc99efee63403b1204c9a311e9
Production <chris@scholance.com>
2018-03-21 00:06:05 -0400

    Testing out authentication stuff

--- handler.js
+++ handler.js
@@ -96,72 +96,75 @@ module.exports.listProjects = (event, context, callback) => {
  * @param callback
  */
 module.exports.createProject = (event, context, callback) => {
-    mongoose.connect(mongoString);
-    const db = mongoose.connection;
-    let data = {};
-    let errs = {};
-    let project = {};
+    console.log(event)
+    callback(null, {statusCode: 201, body: JSON.stringify(event)});
 
-    data = JSON.parse(event.body);
-
-    // Check to make sure the poster is a Business user
-    if (data.liaison.userType !== 'business') {
-        callback(null, createErrorResponse(403, 'You must be a business user to post a project'));
-    }
-
-    project = new Project({
-        title: data.title,
-        summary: data.summary,
-        liaison: data.liaison._id,
-        organization: {
-            _id: data.organization._id,
-            name: data.organization.name
-        },
-        fullDescription: data.fullDescription,
-        category: data.category
-    });
-
-    errs = project.validateSync();
-
-    if (errs) {
-        console.log(errs);
-        callback(null, createErrorResponse(400, 'Incorrect project data'));
-        db.close();
-        return;
-    }
-
-    db.on('error', () => {callback(null, createErrorResponse(503, 'There was an error connecting to the database'))});
-    db.once('open', () => {
-        project
-            .save()
-            .then(() => {
-                s3.putObject(
-                    {
-                        Bucket: 'scholance-projects',
-                        Key: project._id.toString() + '/'
-                    }, function(err, data) {
-                        if (err) {
-                        console.log(err);
-                        callback(null, createErrorResponse(503, 'There was an error creating the S3 bucket'));
-                    }
-                    else{
-                        console.log(data);
-                        callback(null, {statusCode: 201, body: JSON.stringify({id: project._id})});
-                    }
-                    /*
-                    data = {
-                     Location: "http://examplebucket.s3.amazonaws.com/"
-                    }
-                    */
-                    });
-            })
-            .catch((err) => {
-                callback(null, createErrorResponse(err.statusCode, err.message));
-            })
-            .finally(() => {
-                db.close();
-            });
-    });
+    // mongoose.connect(mongoString);
+    // const db = mongoose.connection;
+    // let data = {};
+    // let errs = {};
+    // let project = {};
+    //
+    // data = JSON.parse(event.body);
+    //
+    // // Check to make sure the poster is a Business user
+    // if (data.liaison.userType !== 'business') {
+    //     callback(null, createErrorResponse(403, 'You must be a business user to post a project'));
+    // }
+    //
+    // project = new Project({
+    //     title: data.title,
+    //     summary: data.summary,
+    //     liaison: data.liaison._id,
+    //     organization: {
+    //         _id: data.organization._id,
+    //         name: data.organization.name
+    //     },
+    //     fullDescription: data.fullDescription,
+    //     category: data.category
+    // });
+    //
+    // errs = project.validateSync();
+    //
+    // if (errs) {
+    //     console.log(errs);
+    //     callback(null, createErrorResponse(400, 'Incorrect project data'));
+    //     db.close();
+    //     return;
+    // }
+    //
+    // db.on('error', () => {callback(null, createErrorResponse(503, 'There was an error connecting to the database'))});
+    // db.once('open', () => {
+    //     project
+    //         .save()
+    //         .then(() => {
+    //             s3.putObject(
+    //                 {
+    //                     Bucket: 'scholance-projects',
+    //                     Key: project._id.toString() + '/'
+    //                 }, function(err, data) {
+    //                     if (err) {
+    //                     console.log(err);
+    //                     callback(null, createErrorResponse(503, 'There was an error creating the S3 bucket'));
+    //                 }
+    //                 else{
+    //                     console.log(data);
+    //                     callback(null, {statusCode: 201, body: JSON.stringify({id: project._id})});
+    //                 }
+    //                 /*
+    //                 data = {
+    //                  Location: "http://examplebucket.s3.amazonaws.com/"
+    //                 }
+    //                 */
+    //                 });
+    //         })
+    //         .catch((err) => {
+    //             callback(null, createErrorResponse(err.statusCode, err.message));
+    //         })
+    //         .finally(() => {
+    //             db.close();
+    //         });
+    // });
 };
 
 
@@ -286,6 +289,96 @@ module.exports.deleteProject = (event, context, callback) => {
 
 
 /**
+ * UPLOAD SUPPLEMENTAL RESOURCE FILE
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.createSupplementalResourceFile = (event, context, callback) => {
+    let project_id = event.pathParameters.project_id;
+    if (!mongoose.Types.ObjectId.isValid(project_id)) {
+        db.close();
+        callback(null, createErrorResponse(400, 'Invalid ObjectId'));
+        return;
+    }
+
+    let data = JSON.parse(event.body);
+    //get the request
+    let name = data.name;
+    let base64String = data.file;
+    if (!name || !base64String) {
+        callback(null, createErrorResponse(400, 'Invalid input'));
+    }
+    //pass the base64 string into a buffer
+    let buffer = new Buffer(base64String, 'base64');
+    let fileMime = fileType(buffer);
+    // Check if the base64 encoded string is a file
+    if (fileMime === null) {
+        callback(null, createErrorResponse(400, 'The string supplied is not a file type'));
+    }
+
+    let fileExt = fileMime.ext;
+    let assetType = fileMime.mime.split('/')[0];
+
+    let bucketName = 'dev-scholance-projects';
+    let filePath = project_id + '/supplemental-resources/';
+    let fileName = name + '.' + fileExt;
+    let fileFullName = filePath + fileName;
+
+    let params = {
+        Bucket: bucketName,
+        Key: fileFullName,
+        Body: buffer
+    };
+
+    let fileLink = 'https://s3.amazonaws.com/' + bucketName + '/' + fileFullName.replace(' ', '+');
+
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            callback(null, createErrorResponse(500, 'Could not upload to S3'));
+        }
+
+        ////
+        mongoose.connect(mongoString);
+        let db = mongoose.connection;
+
+        db.on('error', () => {
+            db.close();
+            callback(null, createErrorResponse(503, 'There was an error connecting to the database'));
+        });
+        db.once('open', () => {
+            Project
+                .findById(project_id)
+                .then((project) => {
+                    if (!project) {
+                        db.close();
+                        callback(null, createErrorResponse(404, 'Project not found'));
+                    } else {
+                        project.update({
+                            $push: {'supplementalResources': {name: fileName, assetType: assetType, uri: fileLink}}
+                        })
+                            .then(() => {
+                                db.close();
+                                callback(null, {statusCode: 201, body: JSON.stringify({'File URL': fileLink})});
+                            })
+                            .catch((err) => {
+                                db.close();
+                                callback(null, createErrorResponse(err.statusCode, err.message));
+                            })
+                    }
+                })
+                .catch((err) => {
+                    db.close();
+                    callback(null, createErrorResponse(err.statusCode, err.message));
+                })
+        });
+        ////
+    })
+};
+
+
+/**
  * PROJECT SIGNUP
  *
  * @param event
@@ -379,7 +472,8 @@ module.exports.projectSignoff = (event, context, callback) => {
     });
 
     let project_id = event.pathParameters.project_id;
-    if (!mongoose.Types.ObjectId.isValid(project_id)) {
+    let user_id = event.pathParameters.user_id;
+    if (!mongoose.Types.ObjectId.isValid(project_id) || !mongoose.Types.ObjectId.isValid(user_id)) {
         db.close();
         callback(null, createErrorResponse(400, 'Invalid ObjectId'));
         return;
@@ -393,12 +487,12 @@ module.exports.projectSignoff = (event, context, callback) => {
                 if (!project) {
                     db.close();
                     callback(null, createErrorResponse(404, 'Project not found'));
-                } else if (!project.entries.some(e => e.student == data._id)) {
+                } else if (!project.entries.some(entry => entry.student == user_id)) {
                     db.close();
                     callback(null, createErrorResponse(404, 'You are not currently signed up for this project'));
                 } else {
                     project.update({
-                        $pull: {'entries': {student: data._id}}
+                        $pull: {'entries': {student: user_id}}
                     })
                         .then(() => {
                             db.close();
@@ -419,97 +513,7 @@ module.exports.projectSignoff = (event, context, callback) => {
 
 
 /**
- * UPLOAD SUPPLEMENTAL RESOURCE FILE
- *
- * @param event
- * @param context
- * @param callback
- */
-module.exports.createSupplementalResourceFile = (event, context, callback) => {
-    let project_id = event.pathParameters.project_id;
-    if (!mongoose.Types.ObjectId.isValid(project_id)) {
-        db.close();
-        callback(null, createErrorResponse(400, 'Invalid ObjectId'));
-        return;
-    }
-
-    let data = JSON.parse(event.body);
-    //get the request
-    let name = data.name;
-    let base64String = data.file;
-    if (!name || !base64String) {
-        callback(null, createErrorResponse(400, 'Invalid input'));
-    }
-    //pass the base64 string into a buffer
-    let buffer = new Buffer(base64String, 'base64');
-    let fileMime = fileType(buffer);
-    // Check if the base64 encoded string is a file
-    if (fileMime === null) {
-        callback(null, createErrorResponse(400, 'The string supplied is not a file type'));
-    }
-
-    let fileExt = fileMime.ext;
-    let assetType = fileMime.mime.split('/')[0];
-
-    let bucketName = 'dev-scholance-projects';
-    let filePath = project_id + '/supplemental-resources/';
-    let fileName = name + '.' + fileExt;
-    let fileFullName = filePath + fileName;
-
-    let params = {
-        Bucket: bucketName,
-        Key: fileFullName,
-        Body: buffer
-    };
-
-    let fileLink = 'https://s3.amazonaws.com/' + bucketName + '/' + fileFullName.replace(' ', '+');
-
-    s3.putObject(params, function(err, data) {
-        if (err) {
-            callback(null, createErrorResponse(500, 'Could not upload to S3'));
-        }
-
-        ////
-        mongoose.connect(mongoString);
-        let db = mongoose.connection;
-
-        db.on('error', () => {
-            db.close();
-            callback(null, createErrorResponse(503, 'There was an error connecting to the database'));
-        });
-        db.once('open', () => {
-            Project
-                .findById(project_id)
-                .then((project) => {
-                    if (!project) {
-                        db.close();
-                        callback(null, createErrorResponse(404, 'Project not found'));
-                    } else {
-                        project.update({
-                            $push: {'supplementalResources': {name: fileName, assetType: assetType, uri: fileLink}}
-                        })
-                            .then(() => {
-                                db.close();
-                                callback(null, {statusCode: 201, body: JSON.stringify({'File URL': fileLink})});
-                            })
-                            .catch((err) => {
-                                db.close();
-                                callback(null, createErrorResponse(err.statusCode, err.message));
-                            })
-                    }
-                })
-                .catch((err) => {
-                    db.close();
-                    callback(null, createErrorResponse(err.statusCode, err.message));
-                })
-        });
-        ////
-    })
-};
-
-
-/**
- * UPLOAD ENTRY ASSET FILE
+ * UPLOAD ENTRY ASSET FROM A FILE
  *
  * @param event
  * @param context
@@ -752,6 +756,84 @@ module.exports.deleteEntryAsset = (event, context, callback) => {
                             db.close();
                             callback(null, createErrorResponse(err.statusCode, err.message));
                         });
+                }
+            })
+            .catch((err) => {
+                db.close();
+                callback(null, createErrorResponse(err.statusCode, err.message));
+            })
+    });
+};
+
+
+/**
+ * PROJECT ENTRY SUBMISSION
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.entrySubmission = (event, context, callback) => {
+    mongoose.connect(mongoString);
+    let db = mongoose.connection;
+    db.on('error', () => {
+        db.close();
+        callback(null, createErrorResponse(503, 'There was an error connecting to the database'));
+    });
+
+    let project_id = event.pathParameters.project_id;
+    if (!mongoose.Types.ObjectId.isValid(project_id)) {
+        db.close();
+        callback(null, createErrorResponse(400, 'Invalid ObjectId'));
+        return;
+    }
+
+    let data = JSON.parse(event.body);
+
+    db.once('open', () => {
+        Project
+            .findById(project_id)
+            .then((project) => {
+                if (!project) {
+                    db.close();
+                    callback(null, createErrorResponse(404, 'Project not found'));
+                } else if (data.userType !== 'student') {
+                    db.close();
+                    callback(null, createErrorResponse(403, 'You must be a student to sign up for a project'));
+                } else if (project.entries.some(element => element.student == data._id)) {
+                    db.close();
+                    callback(null, createErrorResponse(409, 'You are already signed up for this project'));
+                } else {
+                    project.update({
+                        $push: {'entries': {student: data._id}}
+                    })
+                        .then(() => {
+                            s3.putObject(
+                                {
+                                    Bucket: 'scholance-users',
+                                    Key: data._id + '/projects/' + project._id.toString() + '/'
+                                }, function(err, data) {
+                                    if (err) {
+                                        console.log(err);
+                                        callback(null, createErrorResponse(503, 'There was an error creating the S3 bucket'));
+                                    }
+                                    else{
+                                        console.log(data);
+                                        callback(null, {statusCode: 201, body: JSON.stringify({id: project._id})});
+                                    }
+                                    /*
+                                    data = {
+                                     Location: "http://examplebucket.s3.amazonaws.com/"
+                                    }
+                                    */
+                                });
+                            db.close();
+                            callback(null, {statusCode: 204});
+                        })
+                        .catch((err) => {
+                            db.close();
+                            callback(null, createErrorResponse(err.statusCode, err.message));
+                        })
                 }
             })
             .catch((err) => {
